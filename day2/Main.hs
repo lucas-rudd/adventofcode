d9265321bab859484ef3c189986097b74c78ff6b
Lucas Rudd <lucas.rudd@LRUDD-MAC.fourwindsinteractive.com>
2019-12-10 10:16:04 -0700

    part 1 in haskell

---
+++ day2/Main.hs
@@ -0,0 +1,38 @@
+import qualified Data.Text as T
+import Debug.Trace
+import qualified Data.Sequence as Sequence
+import qualified Data.Foldable as Foldable
+
+-- the first two numbers after the Opcode tells you which position, the third iindicates the output position
+-- Opcode 1 means add
+-- Opcode 2 means multiply
+-- Opcode 99 means end
+-- any other Opcode means an error occurred
+
+main :: IO ()
+main = do  
+        output <- (readInputFile "input.txt")
+        let computedIntcode = intcodeComputation 0 output
+        print (computedIntcode!!0)
+
+intcodeComputation :: Int -> [Int] -> [Int]
+intcodeComputation start input = do
+        traceId ("start " ++ show start)
+        traceId ("input " ++ show input)
+        traceId ("input at start " ++ show (input!!(start)))
+        traceId ("input at start + 1: " ++ show (input!!(start+1)))
+        traceId ("input at start + 2: " ++ show (input!!(start+2)))
+        traceId ("replacing value at start + 3: " ++ show (input!!(start+3)))
+        case input!!(start) of
+                1  -> intcodeComputation (start+4) (Foldable.toList (Sequence.update (input!!(start+3)) ((input!!(input!!(start+1))) + (input!!(input!!(start+2)))) (Sequence.fromList input)))
+                2  -> intcodeComputation (start+4) (Foldable.toList (Sequence.update (input!!(start+3)) ((input!!(input!!(start+1))) * (input!!(input!!(start+2)))) (Sequence.fromList input)))
+                99 -> input
+                _  -> error "An error occurred"
+
+        
+
+readInputFile :: FilePath -> IO [Int]
+readInputFile fileName = do
+        contents <- readFile fileName
+        let singlewords = Prelude.map T.unpack (T.splitOn (T.pack ",") (T.pack contents))
+        return (Prelude.map read singlewords)
